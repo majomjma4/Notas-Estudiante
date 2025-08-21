d5f243d5fc33cd9289a1629bfd233024d4225a9a
Majo <monterosmarijo2004@gmil.com>
2025-08-21 17:46:26 -0500

    Creación de bucle para ver los aprobados y reprobados

--- notas.py
+++ notas.py
@@ -1,5 +1,13 @@
 estudiantes = [("Mateo", 92), ("Carlos", 75), ("Ana", 46), ("Damian", 57), ("Paula", 85)]
 aprobados = []
-reprobado = []
+reprobados = []
 
+
+for nombres, notas in estudiantes:
+    if notas >= 70 and notas <= 100:
+        aprobados.append((nombres, notas))
+    elif notas < 70:
+        reprobados.append((nombres, notas))
+    else:
+        print(f"Nota invalida para {nombres}")
 
